83cb812f40bf8dc884350d6efda48bb0d7aa0c98
1908shital <1908shitalchaudhari@gmail.com>
2025-07-26 20:35:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { useState } from 'react';
 
-function App() {
+function TODO() {
+  const [initial, setInitial] = useState('');
+  const [data, setData] = useState([]);
+  const [Search, setSearch] = useState('');
+
+  const getInput = (event) => {
+    setInitial(event.target.value);
+  };
+
+  const takeData = () => {
+    let Store = [...data, initial];
+    setData(Store);
+    setInitial('');
+  };
+
+  const deleteTask = (index) => {
+    const updatedData = data.filter((_, i) => i !== index);
+    setData(updatedData);
+  };
+
+  const filteredData = data.filter((task) =>
+    task.toLowerCase().includes(Search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='SearchTask'>
+  <div className='SearchInputWrapper'>
+    <i className='fa fa-search searchIcon'></i>
+    <input
+      className='SearchInput'
+      type='text'
+      placeholder='Search task'
+      value={Search}
+      onChange={(e) => setSearch(e.target.value)}
+    />
+  </div>
+</div>
+
+
+      <div className='inputTask'>
+        <input
+          className='TaskInput'
+          type='text'
+          placeholder='My task'
+          value={initial}
+          onChange={getInput}
+        />
+        <button className='Added' onClick={takeData}>
+          ADD MyTask
+        </button>
+      </div>
+
+      {filteredData.map((currVal, index) => {
+        return (
+          <>
+            <div className='TaskData'>
+              <p>{currVal}</p>
+              <i
+                id='deleteIcon'
+                style={{ color: 'black' }}
+                onClick={() => deleteTask(data.indexOf(currVal))}
+                className='fa-solid fa-trash'
+              ></i>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 }
 
-export default App;
+export default TODO;
---
+++ src/TodoList.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const TodoList=() =>{
+    const [todo,setTodo]=useState([]);
+    const [task,setTask]=useState()
+    
+
+};
